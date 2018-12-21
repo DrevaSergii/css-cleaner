32539f9f9a57f62040cb8f6c1598b82809a1a0a5
s.dreva <drevaserhii@ukr.net>
2018-12-21 22:38:03 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const typescript = require('gulp-typescript');
+const tsProject = typescript.createProject('tsconfig.json');
+const uglify = require('gulp-uglify');
+
+gulp.task('build', () => {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(uglify())
+        .pipe(gulp.dest(tsProject.options.outDir));
+});
